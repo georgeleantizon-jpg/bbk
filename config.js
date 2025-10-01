5dffa1ee7e574f8ef41cbddcd33d08445f9bb38e
georgeleantizon-jpg <georgeleantizon@gmail.com>
2025-10-01 14:39:52 +0800

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ window.__firebase_config = JSON.stringify({
   apiKey: "AIzaSyAY2GPRaHbU7vfA8J79mewyo2u6qHr-a6s",
   authDomain: "buobuo-ka-81320.firebaseapp.com",
   projectId: "buobuo-ka-81320",
-  storageBucket: "buobuo-ka-81320.firebasestorage.app",
+  storageBucket: "buobuo-ka-81320.appspot.com",
   messagingSenderId: "687871046289",
   appId: "1:687871046289:web:e8efb15aa500add7164530"
 });
